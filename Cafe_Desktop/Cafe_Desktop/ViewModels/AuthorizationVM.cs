b43a6b3459aa8df4dce73ebc7f92068c2e97a1c5
Kawodeda <konserva_25@mail.ru>
2022-03-12 16:50:50 +0700

    AuthorizationVM started

---
+++ Cafe_Desktop/Cafe_Desktop/ViewModels/AuthorizationVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Cafe_Desktop.Models;
+
+namespace Cafe_Desktop.ViewModels
+{
+    public class AuthorizationVM : Notifier
+    {
+        private string _username;
+        private RelayCommand _loginCommand;
+        private RelayCommand _exitCommand;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand LoginCommand
+        {
+            get
+            {
+                return _loginCommand ?? 
+                    (_loginCommand = new RelayCommand((x) =>
+                    { 
+
+                    }));
+            }
+        }
+
+        public RelayCommand ExitCommand
+        {
+            get
+            {
+                return _exitCommand ??
+                    (_exitCommand = new RelayCommand((x) =>
+                    {
+                        App.Current.Shutdown();
+                    }));
+            }
+        }
+    }
+}
---
+++ Cafe_Desktop/Cafe_Desktop/ViewModels/Notifier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Cafe_Desktop.ViewModels
+{
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
